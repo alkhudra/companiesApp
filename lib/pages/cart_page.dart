a5653e750d9c6d09a3994a10fe0294aa9f8d382c
Somaru-chan <salmetaha@gmail.com>
2022-02-02 19:59:03 +0300

    Avocado animated image

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -3,6 +3,7 @@ import 'package:khudrah_companies/Constant/locale_keys.dart';
 import 'package:khudrah_companies/designs/appbar_design.dart';
 import 'package:khudrah_companies/designs/drawar_design.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:lottie/lottie.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({ Key? key }) : super(key: key);
@@ -16,6 +17,16 @@ class _CartPageState extends State<CartPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       //add body
+      body: Center(
+        child: Container(
+          child: Lottie.asset(
+              'images/anim_avocado.json',
+              width: 300,
+              height: 150,
+              fit: BoxFit.fill,
+          ),
+        ),
+      ),
       // endDrawer: drawerDesign(context),
       appBar: bnbAppBar(context, LocaleKeys.cart.tr()),
     );
