bdcac04cbefeaadd142387712a5ff180e1963864
manal <mmsaj000@hotmail.com>
2022-02-07 15:27:39 +0300

    delete from cart not done

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -7,13 +7,17 @@ import 'package:khudrah_companies/Constant/locale_keys.dart';
 import 'package:khudrah_companies/designs/appbar_design.dart';
 import 'package:khudrah_companies/designs/drawar_design.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:khudrah_companies/dialogs/message_dialog.dart';
+import 'package:khudrah_companies/dialogs/progress_dialog.dart';
 import 'package:khudrah_companies/helpers/custom_btn.dart';
 import 'package:khudrah_companies/helpers/pref/shared_pref_helper.dart';
+import 'package:khudrah_companies/helpers/snack_message.dart';
 import 'package:khudrah_companies/network/API/api_response.dart';
 import 'package:khudrah_companies/network/API/api_response_type.dart';
 import 'package:khudrah_companies/network/helper/exception_helper.dart';
 import 'package:khudrah_companies/network/helper/network_helper.dart';
 import 'package:khudrah_companies/network/models/cart/success_cart_response_model.dart';
+import 'package:khudrah_companies/network/models/message_response_model.dart';
 import 'package:khudrah_companies/network/models/product/product_model.dart';
 import 'package:khudrah_companies/network/repository/cart_repository.dart';
 import 'package:khudrah_companies/resources/custom_colors.dart';
@@ -28,13 +32,14 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
-static String language  ='ar';
-
-@override
+  static String language = 'ar';
+  bool isTrashBtnEnabled = true;
+  static String productId = '';
+  @override
   void initState() {
     super.initState();
-    setValue();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,110 +58,6 @@ static String language  ='ar';
         },
       ),
       appBar: bnbAppBar(context, LocaleKeys.cart.tr()),
-      // bottomNavigationBar: BottomAppBar(
-      //   child: Container(
-      //     //Change height to be adaptable
-      //     height: MediaQuery.of(context).size.height*0.14,
-      //     child: Column(
-      //       mainAxisAlignment: MainAxisAlignment.start,
-      //       children: [
-      //         SizedBox(height: 4,),
-      //         Row(
-      //           mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //           children: [
-      //             //subtotal
-      //             Container(
-      //               margin: EdgeInsets.symmetric(vertical: 5),
-      //               child: RichText(
-      //                 text: TextSpan(
-      //                   style: TextStyle(
-      //                     fontSize: 15,
-      //                     fontWeight: FontWeight.w500,
-      //                     fontFamily: 'Almarai',
-      //                     color: CustomColors().primaryGreenColor,
-      //                   ),
-      //                   children: <TextSpan> [
-      //                     TextSpan(text: LocaleKeys.subtotal.tr() + ': ',
-      //                     style: TextStyle(
-      //                       color: CustomColors().darkBlueColor
-      //                     )),
-      //                     TextSpan(text: ' $subtotal SAR',
-      //                     style: TextStyle(
-      //                       color: CustomColors().primaryGreenColor,
-      //                       fontFamily: 'Almarai',
-      //                     )),
-      //                   ]
-      //                 ),
-      //               ),
-      //             ),
-      //             //vat
-      //             Container(
-      //               margin: EdgeInsets.symmetric(vertical: 5),
-      //               child: RichText(
-      //                 text: TextSpan(
-      //                   style: TextStyle(
-      //                     fontSize: 15,
-      //                     fontWeight: FontWeight.w500,
-      //                     fontFamily: 'Almarai',
-      //                     color: CustomColors().primaryGreenColor,
-      //                   ),
-      //                   children: <TextSpan> [
-      //                     TextSpan(text: LocaleKeys.vat.tr() + ': ',
-      //                     style: TextStyle(
-      //                       color: CustomColors().darkBlueColor,
-      //                       fontFamily: 'Almarai',
-      //                     )),
-      //                     TextSpan(text: ' $vat SAR',
-      //                     style: TextStyle(
-      //                       color: CustomColors().primaryGreenColor,
-      //                     )),
-      //                   ]
-      //                 ),
-      //               ),
-      //             ),
-      //           ],
-      //         ),
-      //         SizedBox(height: 5,),
-      //         // Divider(
-      //         //   thickness: 1,
-      //         // ),
-      //         Row(
-      //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //           children: [
-      //             //total
-      //             Container(
-      //               margin: EdgeInsets.symmetric(vertical: 5),
-      //               child: RichText(
-      //                 text: TextSpan(
-      //                   style: TextStyle(
-      //                     fontSize: 19,
-      //                     fontWeight: FontWeight.w600,
-      //                     fontFamily: 'Almarai',
-      //                     color: CustomColors().primaryGreenColor,
-      //                   ),
-      //                   children: <TextSpan> [
-      //                     TextSpan(text: LocaleKeys.total.tr() + ': ',
-      //                     style: TextStyle(
-      //                       color: CustomColors().primaryGreenColor,
-      //                     )),
-      //                     TextSpan(text: ' $total SAR',
-      //                     style: TextStyle(
-      //                       color: CustomColors().primaryGreenColor,
-      //                     )),
-      //                   ]
-      //                 ),
-      //               ),
-      //             ),
-      //             //checkout button
-      //             Container(
-      //               child: greenBtn(LocaleKeys.checkout.tr(), EdgeInsets.symmetric(vertical: 4), () {}),
-      //             ),
-      //           ],
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-      // ),
     );
   }
 
@@ -182,33 +83,37 @@ static String language  ='ar';
 
   Widget listDesign(BuildContext context, SuccessCartResponseModel? model) {
     int price = model!.userCart!.totalCartPrice!;
-    double subtotal =3;
-    double vat =5;
-    num total = model.userCart!.hasDiscount! == true ?  model.userCart!.priceAfterDiscount! :
-    model.userCart!.totalCartPrice!;
+    double subtotal = 3;
+    double vat = 5;
+    num total = model.userCart!.hasDiscount! == true
+        ? model.userCart!.priceAfterDiscount!
+        : model.userCart!.totalCartPrice!;
     double price_vat = 27.2;
     double discount = 20;
+
     return SlidingUpPanel(
       body: ListView.builder(
         itemBuilder: (context, index) {
-          return Slidable(
+          return /*Slidable(
               endActionPane: ActionPane(
                 key: const ValueKey(0),
                 motion: const BehindMotion(),
                 dismissible: DismissiblePane(onDismissed: () {}),
                 children: [
                   SlidableAction(
-                    onPressed: deleteFromCart(),
+                    onPressed: deleteFromCart,
                     backgroundColor: CustomColors().redColor,
                     // foregroundColor: CustomColors().primaryWhiteColor,
                     icon: Icons.delete,
                     label: LocaleKeys.delete_from_cart.tr(),
                   )
+
                 ],
               ),
-              child: cartTile(model!.userCart!.cartProductsList! ,index));
+              child: */
+              cartTile(model.userCart!.cartProductsList!, index);
         },
-        itemCount: model!.userCart!.cartProductsList!.length,
+        itemCount: model.userCart!.cartProductsList!.length,
       ),
       minHeight: 60,
       maxHeight: 270,
@@ -403,20 +308,23 @@ static String language  ='ar';
   }
 
   //--------
-  Widget cartTile(List<CartProductsList?> list, int index)  {
-    ProductsModel model  =  list[index]!.productDto!;
+  Widget cartTile(List<CartProductsList?> list, int index) {
+    ProductsModel model = list[index]!.productDto!;
     Size size = MediaQuery.of(context).size;
     double scWidth = size.width;
     double scHeight = size.height;
 
-    num price = model.hasSpecialPrice == true ?
-    model.specialPrice! : model.originalPrice!;
+    num price = model.hasSpecialPrice == true
+        ? model.specialPrice!
+        : model.originalPrice!;
     String? name = language == 'ar' ? model.arName : model.name;
-    String? category = language == 'ar' ? model.arCategoryName : model.categoryName;
-    String image = model.image != null ?ApiConst.images_url + model.image! :
-    'images/green_fruit.png';
+    String? category =
+        language == 'ar' ? model.arCategoryName : model.categoryName;
+    String image = model.image != null
+        ? ApiConst.images_url + model.image!
+        : 'images/green_fruit.png';
 
-    num userQty =  list[index]!.userProductQuantity!;
+    num userQty = list[index]!.userProductQuantity!;
 
     return ListTile(
       title: Column(
@@ -557,17 +465,22 @@ static String language  ='ar';
                     width: 10,
                   ),
                   //Delete icon
-                  Container(
-                    alignment: Alignment.center,
-                    width: 2,
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.arrow_back_ios,
-                        color: CustomColors().darkGrayColor,
+                  GestureDetector(
+                    onTap: (){
+                      deleteFromCart(context, model.productId);
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 2,
+                      child: IconButton(
+
+                        icon: Icon(
+                          Icons.delete,
+                          color: CustomColors().redColor,
+                        ),
+                        padding: EdgeInsets.zero,
+                        constraints: BoxConstraints(), onPressed: () {  },
                       ),
-                      padding: EdgeInsets.zero,
-                      constraints: BoxConstraints(),
                     ),
                   ),
                   // Container(
@@ -606,11 +519,36 @@ static String language  ='ar';
     );
   }
 
-  //TODO: Define delete method
-  deleteFromCart() {}
+  //--------
+  void deleteFromCart(BuildContext context, String? productId) async {
+    if (isTrashBtnEnabled) {
+      isTrashBtnEnabled = false;
+      showLoaderDialog(context);
+      //----------start api ----------------
 
-  void setValue()async {
+      Map<String, dynamic> headerMap = await getHeaderMap();
 
-  language = await PreferencesHelper.getSelectedLanguage;
+      CartRepository cartRepository = CartRepository(headerMap);
+      ApiResponse apiResponse;
+
+      apiResponse = await cartRepository.deleteProductFromCart(productId!);
+
+      if (apiResponse.apiStatus.code == ApiResponseType.OK.code) {
+        MessageResponseModel model =
+            MessageResponseModel.fromJson(apiResponse.result);
+        Navigator.pop(context);
+        isTrashBtnEnabled = true;
+
+        showSuccessMessage(context, model.message!);
+      } else {
+        Navigator.pop(context);
+        isTrashBtnEnabled = true;
+        showErrorMessageDialog(context, apiResponse.message);
+      }
+    }
+  }
+
+  void setValue() async {
+    language = await PreferencesHelper.getSelectedLanguage;
   }
 }
