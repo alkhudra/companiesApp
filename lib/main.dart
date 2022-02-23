782c8979f3f6c743469ba8817428d3c2b2a6d646
Somaru-chan <salmetaha@gmail.com>
2022-02-23 22:20:39 +0300

    Firebase Local Notifications initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:khudrah_companies/pages/auth/login_page.dart';
 import 'package:khudrah_companies/pages/branch/add_brunches_page.dart';
 import 'package:khudrah_companies/pages/dashboard.dart';
@@ -11,9 +13,29 @@ import 'package:khudrah_companies/router/custom_route.dart';
 import 'package:khudrah_companies/router/route_constants.dart';
 import 'helpers/pref/shared_pref_helper.dart';
 
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', //id
+  'High Importance Notifications', //title
+  description: 'This channel is used for important notifications',
+  importance: Importance.high,
+  playSound: true
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('A bg msg just showed up : ${message.messageId}');
+}
+
+
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await EasyLocalization.ensureInitialized();
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(
     EasyLocalization(
@@ -22,12 +44,34 @@ Future<void> main() async {
         fallbackLocale: Locale('en'),
         child: MyApp()),
   );
+
+
+  await flutterLocalNotificationsPlugin
+  .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+  ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true
+  );
 }
 
-Future init() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-}
+// Future init() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // await flutterLocalNotificationsPlugin
+  // .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+  // ?.createNotificationChannel(channel);
+
+  // await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+  //   alert: true,
+  //   badge: true,
+  //   sound: true
+  // );
+// }
 
 class MyApp extends StatefulWidget {
   const MyApp({
@@ -41,10 +85,79 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   static  bool isUserFirstLogin=false;
   static  bool isUserLoggedIn=true;
+  static int counter = 0;
   @override
   void initState() {
     super.initState();
     setValues();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if(notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode, 
+          notification.title, 
+          notification.body, 
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              channelDescription: channel.description,
+              color: CustomColors().primaryGreenColor,
+              playSound: true,
+              //TODO: Add custom icon
+              icon: '@mipmap/ic_launcher'
+            )
+          )
+        );
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published');
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if(notification != null && android != null) {
+        showDialog(context: context,
+          builder: (_) {
+            return AlertDialog(
+              title: Text('Notification.title'),
+              content:  SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(notification.body!)
+                  ],
+                ),
+              ),
+            );
+          });
+      }
+    });
+  }
+
+  void showNotification() {
+    setState(() {
+      counter++;
+    });
+
+    flutterLocalNotificationsPlugin.show(
+      0, 
+      'Testing $counter', 
+      'How you doin?', 
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id, 
+          channel.name,
+          channelDescription: channel.description,
+          importance: Importance.high,
+          color: CustomColors().primaryGreenColor,
+          playSound: true,
+          //TODO: Add custom icon
+          icon: '@mipmap/ic_launcher'
+        )
+      ));
   }
 
   @override
@@ -62,6 +175,28 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.green,
       ),
       home: getRout(),
+      // home: tempHome(),
+    );
+  }
+
+  //replace getRout widget with tempHome to test local notifs
+  Widget tempHome() {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Testing Notifications'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: TextButton(
+              child: Text('Send local Notif'),
+              onPressed: showNotification,
+            ),
+          ),
+        ],
+      ),
     );
   }
 
