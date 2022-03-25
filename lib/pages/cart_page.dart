38570c975b61cffb1eec1ac043d21d88d2d9e300
Somaru-chan <salmetaha@gmail.com>
2022-03-26 00:54:12 +0300

    Slide Up Panel height edit

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -164,7 +164,7 @@ class _CartPageState extends State<CartPage> {
           ),
         ),
         minHeight: scHeight * 0.07,
-        maxHeight: hasDiscount! ? scHeight * 0.39 : 235,
+        maxHeight: hasDiscount! ? scHeight * 0.39 : 220,
         borderRadius: BorderRadius.only(
           topLeft: Radius.circular(40),
           topRight: Radius.circular(40),
